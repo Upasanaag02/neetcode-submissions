8abe6811ff51c46be270c5f0be5ebe6a8b4ff9fc
upasana agrawal <upasanaag32@gmail.com>
2026-06-23 12:22:06 +0545

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+           stack<int>st;
+           st.push(0);
+           vector <int> result(temperatures.size(), 0);
+           for(int i=1;i<temperatures.size();i++){
+                while(!st.empty() && temperatures[st.top()]<temperatures[i]){
+                     result[st.top()]=i-st.top();
+                     st.pop();
+                }
+                st.push(i);
+           }
+           return result;
+    }
+};
