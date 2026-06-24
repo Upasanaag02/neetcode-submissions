7b169d2dd1db14b2ec53483ab702bf8264a9e667
upasana agrawal <upasanaag32@gmail.com>
2026-06-24 15:52:41 +0545

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int i=0;i<matrix.size();i++){
+            int l=0;
+            int r=matrix[i].size()-1;
+            bool ans=false;
+            while(l<=r){
+                int mid=l+(r-l)/2;
+                if(matrix[i][mid]>target){
+                    r=mid-1;
+
+                }
+                else if(matrix[i][mid]<target){
+                    l=mid+1;
+                }
+                else{
+                    ans=true;
+                    break;
+                }
+            }
+            if(ans){
+                return true;
+            }
+        }
+        return false;
+    }
+};
