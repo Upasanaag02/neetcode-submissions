5320da209a0d128277a1e2821c315c7e4d3f767e
upasana agrawal <upasanaag32@gmail.com>
2026-07-10 11:10:18 +0545

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void dfs(vector<int>&nums,int index){
+        if(index==nums.size()){
+            ans.push_back(nums);
+        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            dfs(nums,index+1);
+            swap(nums[i],nums[index]); 
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        dfs(nums,0);
+        return ans;
+    }
+};
