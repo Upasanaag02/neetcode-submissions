3e6d1ef05980a79d30f9874765fb34a10ec74a1e
upasana agrawal <upasanaag32@gmail.com>
2026-07-24 19:56:39 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev2=1;
+        int prev1=1;
+        for(int i=2;i<=n;i++){
+            int curri=prev1+prev2;
+            prev2=prev1;
+            prev1=curri;
+        }
+          return prev1;
+    }
+  
+};
